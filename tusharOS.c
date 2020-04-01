bb25400d81268880d7641fdeaaa5adc47ec23f71
Tushar Rao <57287141+tusharrao6638@users.noreply.github.com>
2020-04-01 21:51:52 +0530

    Update tusharOS.c

--- tusharOS.c
+++ tusharOS.c
@@ -6,10 +6,10 @@ int main()
 	r = 3; 
 	int alloc[5][3] = { { 0, 1, 0 }, { 2, 0, 0 },  { 3, 0, 2 }, { 2, 1, 1 }, { 0, 0, 2 } };
 	int max[5][3] = { { 7, 5, 3 }, { 3, 2, 2 },  { 9, 0, 2 }, { 2, 2, 2 }, { 4, 3, 3 } };  
-	int avail[3] = { 3, 3, 2 }; 
-	nt f[p], ans[p], ind = 0; 
-	for (k = 0; k < p; k++)
-        { 
+	int avail[3] = { 3, 3, 2 };  
+	int f[p], ans[p], ind = 0; 
+	for (k = 0; k < p; k++) 
+	 { 
 		f[k] = 0; 
 	} 
 	int need[p][r]; 
@@ -20,13 +20,13 @@ int main()
 	} 
 	int y = 0; 
 	for (k = 0; k < 5; k++)
-		 { 
+	 { 
 		for (i = 0; i < p; i++) 
 			{ 
 			if (f[i] == 0)
 			 { 
 				int flag = 0; 
-				for (j = 0; j < r; j++){ 
+				for (j = 0; j < r; j++) { 
 					if (need[i][j] > avail[j]){ 
 						flag = 1; 
 						break; 
@@ -50,6 +50,7 @@ int main()
 
 	return (0); 
 } 
+
  
 
 
